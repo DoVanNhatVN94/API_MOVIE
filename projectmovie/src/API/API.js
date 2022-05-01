1668353a51aee3ea01a9bf80d74a00ce79402e62
Nam <phamhainamkt@gmail.com>
2022-05-01 21:48:03 +0700

    Chức năng thêm phim

--- projectmovie/src/API/API.js
+++ projectmovie/src/API/API.js
@@ -2,16 +2,21 @@ import { http } from "../util/setting"
 
 export const urlChiTietPhim = "https://movienew.cybersoft.edu.vn/api/QuanLyPhim/LayThongTinPhim?MaPhim="
 
- class Manager {
-    getDSPhim= ()=>{
+class Manager {
+    getDSPhim = () => {
         return http.get(`/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP02`)
     }
-    getDSBanner=()=>{
+    getDSBanner = () => {
         return http.get('/api/QuanLyPhim/LayDanhSachBanner')
     }
-    postNDlogin=()=>{
+    postNDlogin = () => {
         return http.post('/api/QuanLyNguoiDung/ThongTinTaiKhoan')
-    }   
+    }
+
+    // devNam
+    themPhimUploadHinh = (formData) => {
+        return http.post('/api/QuanLyPhim/ThemPhimUploadHinh', formData)
+    }
 }
-const  manager = new Manager()
+const manager = new Manager()
 export default manager
