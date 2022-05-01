06bd6e99a878c610666a3c14f36ddbe360598dc6
Nam <phamhainamkt@gmail.com>
2022-05-01 22:40:08 +0700

    conflict code dev Nam

--- projectmovie/src/API/API.js
+++ projectmovie/src/API/API.js
@@ -18,11 +18,8 @@ class Manager {
         return http.post('/api/QuanLyPhim/ThemPhimUploadHinh', formData)
     }
     
-<<<<<<< HEAD
     // devNam
 
-=======
->>>>>>> devNam5
     getTTlichChieuPhim=(maPhim)=>{
         return http.get(`/api/QuanLyRap/LayThongTinLichChieuPhim?MaPhim=${maPhim}`)
     }  
