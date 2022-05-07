7595f86dcdc2092043965090efda775ffde47107
Nam <phamhainamkt@gmail.com>
2022-05-07 15:06:09 +0700

    devNam6 thêm code mới

--- projectmovie/src/page/AdminPage/AdminPage.js
+++ projectmovie/src/page/AdminPage/AdminPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AdminPage() {
   return (
     <div>
-      <h2 className='mt-4'>Trang của quản trị viên</h2>
+      <h2 className='mt-4'>Trang quản trị viên</h2>
     </div>
   )
 }
